643e072f88174fbe6ad814893f0ce18f9d628867
Andrew Taylor <aglt704@me.com>
2024-02-03 17:18:39 +0000

    Deleted old code

--- TestDataLoad.py
+++
@@ -1,9 +0,0 @@
-import CreditObjects
-import pandas as pd
-#from csv import DictReader
-
-#with open('Dummy_loan_data.csv') as f:
-#    reader = DictReader(f)
-#    for row in reader:
-#        facilities.append(Facility())
-
