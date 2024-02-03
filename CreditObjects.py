bf0bb1d14ae73a276e296236d7b00c603d3e6c5b
Andrew Taylor <aglt704@me.com>
2024-02-03 17:48:18 +0000

    Updated risk weight formula for small firms

--- CreditObjects.py
+++ CreditObjects.py
@@ -38,7 +38,10 @@ class Facility():
         return ecl
     
     def risk_weight(self):
+        # Note this just does the Corporate RWA calc today - needs expanding to cover Retail
         R = (0.12 * (1 - np.exp(-50*self.customer.probdef))/(1-np.exp(-50))) + (0.24 * (1-(1 - np.exp(-50*self.customer.probdef))/(1-np.exp(-50))))
+        if self.customer.turnover <= 50:
+            R += -0.04 * (1 - ((max(self.customer.turnover, 5) - 5) / 45))
         b = (0.11852 - (0.05478 * np.log(self.customer.probdef)))**2
         matadj = (1 + ((self.effmaturity  - 2.5) * b))/(1 - (1.5 * b))
         temp = norm.ppf(self.customer.probdef)/np.sqrt(1-R)+np.sqrt(R/(1-R))*norm.ppf(0.999) 
@@ -52,13 +55,14 @@ class Facility():
         return rwa 
     
 class Customer():
-    def __init__(self, customerid, probdef, sic_code, country, name, parent = None):
+    def __init__(self, customerid, probdef, sic_code, country, name, turnover, parent = None):
         self.customerid = customerid
         self.probdef = probdef
         self.sic_code = sic_code
         self.country = country
         self.name = name
         self.parent = parent
+        self.turnover = turnover / 1000000
         self.facility_list = []
 
     def add_facility(self, facility):
@@ -80,9 +84,10 @@ data = pd.read_csv('Dummy_loan_data.csv')
 data['Start_date'] = pd.to_datetime(data['Start_date'])
 data['Maturity_date'] = pd.to_datetime(data['Maturity_date'])
 data['Limit'] = data['Limit'].astype('float')
+data['Turnover'] = data['Turnover'].astype('float')
 data['Drawn_balance'] = data['Drawn_balance'].astype('float')
 
-customerdata = data[['Customer ID', 'Name', 'SIC_Code', 'Country', 'Parent', 'PD']]
+customerdata = data[['Customer ID', 'Name', 'SIC_Code', 'Country', 'Parent', 'PD', 'Turnover']]
 customerdata = customerdata.drop_duplicates(subset='Customer ID')
 
 facilities = []
@@ -90,7 +95,8 @@ customers = []
 
 for i in customerdata.index:
     customer = Customer(customerdata['Customer ID'][i], customerdata['PD'][i], customerdata['SIC_Code'][i],
-                        customerdata['Country'][i], customerdata['Name'][i],customerdata['Parent'][i])
+                        customerdata['Country'][i], customerdata['Name'][i],customerdata['Turnover'][i],
+                        customerdata['Parent'][i])
     customers.append(customer)
 
 for i in data.index:
@@ -114,7 +120,6 @@ for i in data.index:
 # print(facilities[1].rwa())
 # print(facilities[1].maturity)
 # print(facilities[1].effmaturity)
-# print(facilities[0].risk_weight())
-# print(facilities[1].rwa())
-
+print(facilities[0].risk_weight())
+# print(facilities[6].rwa())
 # print(customers[1].ecl())
